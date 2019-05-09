57d3cf52b13f1ba3fd7dc7b738ed28fe8d1f8eb8
Danielle Emygdio <emygdio.d@gmail.com>
2019-05-09 10:27:35 -0300

    Add routes and rails c content

--- modulo_03/crypto_wallet/app/helpers/application_helper.rb
+++ modulo_03/crypto_wallet/app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+    def brazilian_date(us_date)
+        us_date.strftime("%d/%m/%Y")
+    end
+
+    def application_name
+        "Crypto Wallet App"
+    end
+
+    def translate_environment
+        if Rails.env.development?
+            "desenvolvimento"
+        elsif Rails.env.production?
+            "produção"
+        else
+            "teste"
+        end
+    end
 end
