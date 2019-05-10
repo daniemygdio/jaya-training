de4acb1a4ba4b7782f7b570bb6e294b9257ca341
Danielle Emygdio <emygdio.d@gmail.com>
2019-05-10 13:53:27 -0300

    Add session and cookie example

--- modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
+++ modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @random_number = rand(10)
-
     @my_name = params[:my_name]
+
+    cookies[:curso] = "Curso de Ruby on Rails - Jackson Pires COOKIE"
+    session[:curso] = "Curso de Ruby on Rails - Jackson Pires SESSION"
   end
 end
